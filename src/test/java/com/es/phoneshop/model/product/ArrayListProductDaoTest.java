d5bab7f6624b49bf9bf6bd3e1f3a1ea4965e539b
NegaTiV444 <444negativ@gmail.com>
2019-03-14 21:10:26 +0300

    ArrayListProductDao unit-tests

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,10 +1,17 @@
 package com.es.phoneshop.model.product;
 
+import com.es.phoneshop.model.exceptions.ListAlreadyContainsProductException;
+import com.es.phoneshop.model.exceptions.ListDoesNotContainProductException;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ArrayListProductDaoTest
 {
     private ProductDao productDao;
@@ -17,5 +24,59 @@ public class ArrayListProductDaoTest
     @Test
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Mock
+    private Product product;
+
+    @Test
+    public void testGetProductWithIncorrectId()
+    {
+        boolean isException = false;
+        try {
+            productDao.getProduct(product.getId());
+        } catch (ListDoesNotContainProductException e) {
+            isException = true;
+        }
+        assertTrue(isException);
+    }
+
+    @Test
+    public void testSaveOneProductTwice()
+    {
+        boolean isException = false;
+        try {
+            productDao.save(product);
+            productDao.save(product);
+        } catch (ListAlreadyContainsProductException e) {
+            isException = true;
+        }
+        assertTrue(isException);
+    }
+
+    @Test
+    public void testSaveAndGet()
+    {
+        try {
+            productDao.save(product);
+            assertEquals(product, productDao.getProduct(product.getId()));
+        } catch (ListDoesNotContainProductException | ListAlreadyContainsProductException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testDelete()
+    {
+        boolean isException = false;
+        try {
+            productDao.save(product);
+            productDao.delete(product.getId());
+        } catch (ListAlreadyContainsProductException e) {
+            e.printStackTrace();
+        } catch (ListDoesNotContainProductException e) {
+            isException = true;
+        }
+        assertTrue(!isException);
     }
 }
